d0fc246799fd8ed6e2d0bc031baea4a3726307e2
haanbiit <lsjhb0500@gmail.com>
2021-07-20 17:51:31 +0900

    영화 상세정보 DB저장 (오류)

--- 코드/movie data/movie_info.py
+++ 코드/movie data/movie_info.py
@@ -18,49 +18,52 @@ mc = cursor.fetchall()
 l = []
 new_list = []
 for i in range(len(mc)):
-    movie = mc[i][1]
+    movie = str(mc[i][1])
     header_parms ={"X-Naver-Client-Id":client_id,"X-Naver-Client-Secret":client_secret}
     url = f"https://openapi.naver.com/v1/search/movie.json?query={movie}"
     res=requests.get(url,headers=header_parms)
     data =res.json()
     
-    print(data)
+    #print(data)
+    #print()
+    #print()
 
     
     titles=[]
     links=[]
+    images=[]
     dates=[]
     directors=[]
     actors=[]
     ratings=[]
 
     for i in data['items']:
-        titles.append(i['title'].strip('</b>').replace('<b>','').replace('</b>',''))
-        links.append(i['link'])
-        dates.append(i['pubDate'])
-        directors.append(i['director'].split('|')[0])
-        actors.append(i['actor'].split('|')[:-1])
-        ratings.append(float(i['userRating']))
+        if i['title'].strip('</b>').replace('<b>','').replace('</b>','') == movie:
+            titles.append(i['title'].strip('</b>').replace('<b>','').replace('</b>',''))
+            links.append(i['link'])
+            images.append(i['image'])
+            dates.append(i['pubDate'])
+            directors.append(i['director'].split('|')[0])
+            actors.append(i['actor'].split('|')[:-1])
+            ratings.append(float(i['userRating']))
         
     for j in range(len(titles)):
-        l.append([titles[j], links[j], dates[j], directors[j], actors[j], ratings[j]])
-    
-    
-    #데이터 전처리
-    #title=data['items'][0]['title'].strip('</b>')
-    #link=data['items'][0]['link']
-    #image=data['items'][0]['image']
-    #date=data['items'][0]['pubDate']
-    #director=data['items'][0]['director'].split('|')[0]
-    #actors=data['items'][0]['actor'].split('|')[:-1]
-    #rating=float(data['items'][0]['userRating'])
-    
-    #l.append([title, link, image, date, director, actors, rating])
+        l.append([titles[j], links[j], images[j], dates[j], directors[j], actors[j], ratings[j]])
+
 
 for v in l:
     if v not in new_list:
         new_list.append(v)
-print(new_list)
+#print(new_list)
 
-conn.commit() 
+sql = "INSERT INTO movieinfo (title, link, image, date, director, actors, rating) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+#
+
+for b in new_list: 
+    cursor.execute(sql,b)
+
+conn.commit()
 conn.close()
+
+import pandas as pd
+pd.DataFrame(new_list, columns=['영화 제목','네이버 링크','포스터','개봉 년도','감독','출연진','평점'])
