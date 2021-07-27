ac23e1c8967ed62170e1811ed638f2cee99deba8
DESKTOP-EDOFIHE\eun20 <eunkyung20516@gmail.com>
2021-07-27 16:27:24 +0900

    /movieTable 추가

--- 코드/ott01/server/server.js
+++ 코드/ott01/server/server.js
@@ -5,7 +5,7 @@ const db = require('./db_access/db');
 
 // body-parser 모듈을 express.js에서 자체 제공함
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({extended : true}));
 
 app.get('/movies', (req, res) => {
     db.query("SELECT * FROM movies_db.contents;", (err, data) => {
@@ -28,6 +28,37 @@ app.get('/boxoffice', (req, res) => {
         }
     })
 })
+
+app.get("/movieTable",function(request,response){  
+    db.query('SELECT * from contents', function(err, rows, fields) {  
+    db.end(); 
+    
+    if (!err){  
+        var data="<html><head><title>mysql test</title></head>"  
+        data += "<h1>영화목록</h1>"  
+        data += "<table border=\"1\">"    
+        data += "<tr> <th>content_id</th> <th>title</th>  <th>summary</th> <th>production_country</th> <th>field_genre</th>  <th>screening_date</th> <th>age_imformation</th>  <th>director</th> </tr>"  
+
+        for (var i in rows) {  
+            data += "<tr>"  
+            data += "<td>"+rows[i].content_id +"</td>"  
+            data += "<td>"+rows[i].title+"</td>";  
+            data += "<td>"+rows[i].summary+"</td>";  
+            data += "<td>"+rows[i].production_country+"</td>";  
+            data += "<td>"+rows[i].field_genre+"</td>";  
+            data += "<td>"+rows[i].screening_date+"</td>";  
+            data += "<td>"+rows[i].age_imformation+"</td>";
+            data += "<td>"+rows[i].director+"</td>";
+            data += "</tr>"  
+        }  
+
+        data+="</table></html>"  
+        response.send(data);  
+    }  
+    else  
+        console.log('Error while performing Query.');  
+    });  
+});  
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
