e0dec98b65e250e53455cf58b21c57f290452196
haanbiit <lsjhb0500@gmail.com>
2021-08-06 16:33:52 +0900

    스케줄러 배치파일 수정(대기시간 줄이고 주석 수정)

--- code/get data/crawler.bat
+++ code/get data/crawler.bat
@@ -1,15 +1,15 @@
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_list.py" rem 영화 목록, 영화배우 목록
-timeout /t 60 rem 60초씩 지연
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_links_url.py" rem 영화 link url
-timeout /t 60
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_info.py" rem 영화 상세정보 (국가, 장르, 연령제한, 감독)
-timeout /t 60
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_summary.py" rem 영화 줄거리
-timeout /t 60
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_poster.py" rem 영화 포스터
-timeout /t 60
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_person_filmo.py" rem 영화 배우 필모리스트
-timeout /t 60
-"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_person.py" rem 영화-배우리스트
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_list.py" rem movie list, actor list
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_links_url.py" rem movie link url
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_info.py" rem movie info (country, genre, age, director)
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_summary.py" rem movie summary
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_poster.py" rem movie poster
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_person_filmo.py" rem movie actor filmo list
+timeout /t 1
+"C:\Python\Python39\python.exe" "C:\Users\USER\Documents\OTTWeb\code\get data\n_contents_person.py" rem movie - actor list
 
 pause
