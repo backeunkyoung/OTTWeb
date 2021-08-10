579664e347e4458aa40be9246bc0cdf9ed862da2
haanbiit <lsjhb0500@gmail.com>
2021-08-10 18:15:53 +0900

    플랫폼 정보 가져오기 (db저장 안 됨)

---
+++ code/movie data/platform_link.py
@@ -0,0 +1,44 @@
+#어느 플랫폼에 영화가 있는지
+import requests
+import json
+import pymysql
+from urllib.parse import quote
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+
+conn = pymysql.connect(host='18.188.140.138', user='user01', password=password, db='movies_db', charset='utf8')
+cursor = conn.cursor() 
+
+sql = "SELECT content_id, title FROM contents"
+
+cursor.execute(sql)
+pc = cursor.fetchall()
+
+
+pedia = []
+
+for j in range(len(pc)):
+    #print(pc[j][1])
+    link = 'https://pedia.watcha.com/ko-KR/searches/movies?query='+quote(pc[j][1])
+    #print(link)
+    res = requests.get(link)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    img = soup.find_all("li","css-19dwtmk")
+    for i in range(len(img)):
+        if pc[j][1].replace(" ","") == soup.find_all("li", class_="css-19dwtmk")[i].find("a")['title'].replace(" ",""):
+            pp = soup.find_all("li", class_="css-19dwtmk")[i].find("a")['href']
+            link1 = 'https://pedia.watcha.com'+pp
+            #print(link1)
+            pedia.append([str(pc[j][0]), pc[j][1], link1])
+            break
+
+sql = "INSERT INTO platform (content_pid, title, pedia_link) VALUES (%s, %s, %s)"            
+
+for p in pedia:
+    try:
+        cursor.execute(sql,p)
+        print(p)
+    except:
+        print('안들어감')
+        pass
