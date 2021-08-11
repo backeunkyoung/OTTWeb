88e3dd52d21ab6f268485acefb6a563b40476a13
haanbiit <lsjhb0500@gmail.com>
2021-08-11 18:06:26 +0900

    컨텐츠랑 장르, 컨텐츠랑 국가 테이블

---
+++ code/movie data/content_attribute.py
@@ -0,0 +1,61 @@
+#### content랑 genre 테이블
+import requests
+import json
+import datetime
+import pymysql
+import time
+
+
+#conn = pymysql.connect(host='18.188.140.138', user='user01', password='1111', db='movies_db', charset='utf8')
+cursor = conn.cursor()
+
+sql = "SELECT content_id, title FROM contents"
+
+cursor.execute(sql)
+mc = cursor.fetchall() #content테이블 content_id, title 불러옴
+
+sql = "SELECT * FROM attribute_genres"
+
+cursor.execute(sql)
+g_table = cursor.fetchall() #장르 테이블 불러옴
+
+
+
+for i in range(len(mc)):
+    movieCd = str(mc[i][0]) #검색을 위해 문자열로 바꿈
+
+    #무비코드로 영화 상세정보 크롤링(영진위API)
+    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key='+mykey+'&movieCd='+movieCd
+
+    #time.sleep(3) #3초
+    req = requests.get(url)
+    text = req.text
+
+    d = json.loads(text)
+
+    b=d['movieInfoResult']['movieInfo'] #b = 영화세부정보
+    #print(b)
+    print(b['movieNm'])
+
+    #장르정보 (문자열 하나로 만들지 말고 하나하나 따로 로우로 만들어서 connect에 저장해야 함)
+    for k in b['genres']: #k = 영화 장르정보
+        genre = k['genreNm']
+        check = 0
+        for j in range(len(g_table)): #장르를 코드로 치환
+            if genre == g_table[j][1]:
+                genre_code = g_table[j][0]
+                check = 1
+                break
+        if check == 0: #장르 테이블에 없으면 기타(30)로 저장됨
+            genre_code = 30
+        #print('전:'+str(genre)+' 후:'+str(genre_code))
+        l = [mc[i][0], genre_code]
+        sql = "INSERT INTO connect (content_pid, attribute_num) VALUES (%s, %s)"
+        try:
+            cursor.execute(sql,l)
+        except:
+            print('오류생김')
+            pass
+        
+conn.commit()
+conn.close()
---
+++ code/movie data/content_country.py
@@ -0,0 +1,60 @@
+####content랑 country 테이블
+import requests
+import json
+import datetime
+import pymysql
+import time
+
+
+#conn = pymysql.connect(host='18.188.140.138', user='user01', password='1111', db='movies_db', charset='utf8')
+cursor = conn.cursor()
+
+sql = "SELECT content_id, title FROM contents"
+
+cursor.execute(sql)
+mc = cursor.fetchall() #content테이블 content_id, title 불러옴
+
+sql = "SELECT * FROM production_countrys"
+
+cursor.execute(sql)
+c_table = cursor.fetchall() #국가 테이블 불러옴
+
+
+for i in range(len(mc)):
+    movieCd = str(mc[i][0]) #검색을 위해 문자열로 바꿈
+
+    #무비코드로 영화 상세정보 크롤링(영진위API)
+    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key='+mykey+'&movieCd='+movieCd
+
+    #time.sleep(3) #3초
+    req = requests.get(url)
+    text = req.text
+
+    d = json.loads(text)
+
+    b=d['movieInfoResult']['movieInfo'] #b = 영화세부정보
+    #print(b)
+    print(b['movieNm'])
+    
+    #국가정보 (문자열 하나로 만들지 말고 하나하나 따로 로우로 만들어서 connect2에 저장해야 함)
+    for j in b['nations']: #j = 영화 국가정보
+        nation = j['nationNm']
+        check = 0
+        for j in range(len(c_table)): #국가이름을 코드로 치환
+            if nation == c_table[j][1]:
+                nation_code = c_table[j][0]
+                check = 1
+                break
+        if check == 0: #국가 테이블에 없는 국가는 기타(ee)로 저장됨
+            nation_code = 'ee'
+        #print('전:'+nation+' 후:'+nation_code)
+        l = [mc[i][0], nation_code]
+        sql = "INSERT INTO connect2 (content_pid, nation_code) VALUES (%s, %s)"
+        try:
+            cursor.execute(sql,l)
+        except:
+            print('오류생김')
+            pass
+        
+conn.commit()
+conn.close()
