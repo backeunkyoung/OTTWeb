7b65ad108bbd63b07c1720efad7f7512b647336c
DESKTOP-EDOFIHE\eun20 <eunkyung20516@gmail.com>
2021-08-11 21:29:05 +0900

    검색창에 검색 시 해당 키워드에 맞는 영화 정보만 가져와서 테이블에 보여주기

--- code/ott01/src/components/movie_table/Movie_Table_Body.js
+++ code/ott01/src/components/movie_table/Movie_Table_Body.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 
 function Movie_Table_Body(props) {
-    console.log("Movie_Table_Body props : \n" + JSON.stringify(props.list));
+    // console.log("Movie_Table_Body jjjj props : \n" + JSON.stringify(props));
 
-    const [movies, setMovies] = useState(null);
-
-    useEffect(() => {   // 컴포넌트가 렌더링 될 때마다 특정 작업 실행
-        setMovies(props);
-    }, [movies]);  // movies가 바뀔 때 실행
-
+    const text = _.get(props, "list.list.data");
 
     return(
         <div>
+            {console.log("props : ", JSON.stringify(props.list.list))}
+
             <React.Fragment>
                 {/* React는 렌더링이 화면에 커밋 된 후에 모든 효과를 실행 함*/}
                 {/* 즉, return에서 map을 반복 실행 할 때, movies에 첫 데이터가 아직 안들어와도 렌더링이 실행 된다. => undefined 오류 발생 */}
                 {/* 따라서 movies && 조건을 추가 해 movies에 값이 있으면(true) 실행되도록 한다 */}
-                {movies && movies.data.map(movie =>
+                {text && text.map(movie =>
                     <tr key={movie.content_id}>
                         <td><img src = {movie.poster} width="150" height="250"></img></td>   {/* poster */}
                         <td>{movie.title}</td>   {/* title */}
