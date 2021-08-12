b3a55bba33d6766a8ebb581813da02649bbf6245
haanbiit <lsjhb0500@gmail.com>
2021-08-12 17:00:21 +0900

    컨텐츠랑 배우 테이블 수정 row한 줄 당 배우 한 명 씩

--- code/movie data/content_person.py
+++ code/movie data/content_person.py
@@ -1,8 +1,7 @@
-#content_person
+#content_person 수정 후
 import pymysql
 
-conn = pymysql.connect(host='18.188.140.138', user='user01', password='1111', db='movies_db', charset='utf8') 
-#conn = pymysql.connect(host='localhost', user='root', password='root', db='moviedata', charset='utf8') 
+#conn = pymysql.connect(host='18.188.140.138', user='user01', password='1111', db='movies_db', charset='utf8') 
 cursor = conn.cursor()
 
 sql = "SELECT content_id FROM contents"
@@ -16,36 +15,22 @@ sql = "SELECT * FROM person"
 cursor.execute(sql)
 pc = cursor.fetchall()
 
-sql = "INSERT INTO contents_persons (content_pid, person_pid) VALUES (%s, %s)"
 
-CnP = []
 for i in range(len(mc)):
     print(i)
-    l = ''
-    movieCd = str(mc[i][0]) #무비코드 받아옴
+    movieCd = str(mc[i][0]) #무비코드 받아옴 비교를 위해 문자열로 변환
     
-    count = 0
     for j in range(len(pc)): #검색할 무비코드랑 영화인 필모의 무비코드 비교
-        if movieCd in pc[j][2]: #영화인 필모에 무비코드 있으면 문자열에 추가
-            l += pc[j][0]+', '
-            count += 1
-            print(pc[j][1])
-        if count==30: #30명 이상이면 30명까지만 받음
-            count = 0
-            break
-    l = l.rstrip(', ')
-    print(l)
-    
-    CnP.append([movieCd, l])
-    
-    
-for k in CnP:
-    try:
-        print(k)
-        cursor.execute(sql,k)
-    except:
-        print('안들어감')
-        pass
+        if movieCd in pc[j][2]: #영화인 필모에 무비코드 있으면 content_person테이블에 row추가 해야함
+            person = pc[j][0]
+            print('영화:'+movieCd+' 배우번호:'+person+' 이름:'+pc[j][1])
+            l = [mc[i][0],person]
+            sql = "INSERT INTO contents_persons (content_pid, person_pid) VALUES (%s, %s)"
+            try:
+                cursor.execute(sql,l)
+            except:
+                print('안들어감')
+                pass
     
     
 conn.commit()
